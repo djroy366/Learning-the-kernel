77afd89c471f2870f1a611e285583da9a9e15b48
djroy <djroy366@gmail.com>
2021-08-24 22:42:20 +0530

    Learning the kernel

---
+++ .ex01_simple_module.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/djroy/youtube_stuff/device_driver/ex01_simple_module.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/djroy/youtube_stuff/device_driver/ex01_simple_module.ko /home/djroy/youtube_stuff/device_driver/ex01_simple_module.o /home/djroy/youtube_stuff/device_driver/ex01_simple_module.mod.o ;  true
---
+++ .tmp_versions/ex01_simple_module.mod
@@ -0,0 +1,3 @@
+/home/djroy/youtube_stuff/device_driver/ex01_simple_module.ko
+/home/djroy/youtube_stuff/device_driver/ex01_simple_module.o
+
